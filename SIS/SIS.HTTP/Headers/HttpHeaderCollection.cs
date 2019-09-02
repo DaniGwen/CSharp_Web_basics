43f47f2ed8aa78167354abce39a2e4d2b3b30c59
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-02 13:57:01 +0300

    Update

--- SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
+++ SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -7,23 +7,23 @@ namespace SIS.HTTP.Headers
 {
     public class HttpHeaderCollection : IHttpHeaderCollection
     {
-        private readonly Dictionary<string, HttpHeader> headers;
+        private readonly Dictionary<string, HttpHeader> httpHeaders;
 
         public HttpHeaderCollection()
         {
-            this.headers = new Dictionary<string, HttpHeader>();
+            this.httpHeaders = new Dictionary<string, HttpHeader>();
         }
 
         public void AddHeader(HttpHeader header)
         {
-            this.headers.Add(header.Key, header);
+            this.httpHeaders.Add(header.Key, header);
         }
 
         public bool ContainsHeader(string key)
         {
             bool isContained = true;
 
-            if (!headers.ContainsKey(key))
+            if (!httpHeaders.ContainsKey(key))
             {
                 isContained = false;
             }
@@ -33,9 +33,9 @@ namespace SIS.HTTP.Headers
 
         public HttpHeader GetHeader(string key)
         {
-            if (headers.ContainsKey(key))
+            if (httpHeaders.ContainsKey(key))
             {
-                return (HttpHeader)headers.First(h => h.Key == key).Value;
+                return (HttpHeader)httpHeaders.First(h => h.Key == key).Value;
             }
 
             return null;
@@ -45,11 +45,11 @@ namespace SIS.HTTP.Headers
         {
             var sb = new StringBuilder();
 
-            foreach (var header in this.headers)
+            foreach (var header in this.httpHeaders)
             {
-                sb.Append($"{header.Key} {header.Value}")
+                sb.Append($"{header.ToString()}\r\n");
             }
-            return base.ToString(); 
+            return sb.ToString(); 
         }
     }
 }
