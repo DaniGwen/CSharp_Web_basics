94392debfbc28b0695f99b215743ba1381a0689d
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-09 13:11:55 +0300

    Update

--- SIS/SIS.Web.Server/ConnectionHandler.cs
+++ SIS/SIS.Web.Server/ConnectionHandler.cs
@@ -1,8 +1,63 @@
-﻿using System;
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.HTTP.Responses.Contracts;
+using SIS.Web.Server.Routing;
+using System;
+using System.Net.Sockets;
 
 namespace SIS.WebServer
 {
     public class ConnectionHandler
     {
+        private readonly Socket client;
+
+        private readonly IServerRoutingTable serverRoutingTable;
+
+        public ConnectionHandler(Socket client, IServerRoutingTable serverRoutingTable)
+        {
+            CoreValidator.ThrowIfNull(client, nameof(client));
+            CoreValidator.ThrowIfNull(serverRoutingTable, nameof(serverRoutingTable));
+
+            this.client = client;
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public void ProcessRequest()
+        {
+            try
+            {
+                var httpRequest = this.ReadRequest();
+
+                if (httpRequest != null)
+                {
+                    Console.WriteLine($"Processing {httpRequest.RequestMethod} {httpRequest.Path}...");
+
+                    var httpResponse = this.HandleRequest(httpRequest);
+
+                    this.PrepareResponse(httpResponse);
+                }
+            }
+            catch ()
+            {
+
+                throw;
+            }
+        }
+
+        private HttpRequest ReadRequest()
+        {
+
+        }
+
+        private HttpResponse HandleRequest(IHttpRequest httpRequest)
+        {
+
+        }
+
+        private void PrepareResponse(IHttpResponse httpResponse)
+        {
+
+        }
     }
 }
--- SIS/SIS.Web.Server/Server.cs
+++ SIS/SIS.Web.Server/Server.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using SIS.Web.Server.Routing;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SIS.WebServer
 {
     public class Server
     {
+        private const string LocalhostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+
+        private readonly TcpListener tcpListener;
+
+        private readonly IServerRoutingTable routingTable;
+
+        private bool isRunning;
+
+        public Server(int port, IServerRoutingTable routingTable)
+        {
+            this.port = port;
+            this.routingTable = routingTable;
+            this.tcpListener = new TcpListener(IPAddress.Parse(LocalhostIpAddress), port);
+
+            while (this.isRunning)
+            {
+                Console.WriteLine($"waiting for client...");
+
+                var client = this.tcpListener.AcceptSocket();
+
+                this.Listen(client);
+            }
+        }
+
+        public void Run()
+        {
+            this.tcpListener.Start();
+            this.isRunning = true;
+
+            Console.WriteLine($"Server started at {LocalhostIpAddress}:{this.port}");
+        }
+
+        public async Task Listen(Socket client)
+        {
+            var connectionHandler = new ConnectionHandler(client, this.routingTable);
+            connectionHandler.ProcessRequest();
+        }
     }
 }
