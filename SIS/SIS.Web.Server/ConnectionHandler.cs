fa25f8df921e12f77d401bafdfe3a3e298150782
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-09 16:21:34 +0300

    Update

--- SIS/SIS.Web.Server/ConnectionHandler.cs
+++ SIS/SIS.Web.Server/ConnectionHandler.cs
@@ -1,10 +1,14 @@
 ﻿using SIS.HTTP.Common;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Exceptions;
 using SIS.HTTP.Requests;
 using SIS.HTTP.Responses;
 using SIS.HTTP.Responses.Contracts;
 using SIS.Web.Server.Routing;
+using SIS.WebServer.Result;
 using System;
 using System.Net.Sockets;
+using System.Text;
 
 namespace SIS.WebServer
 {
@@ -38,21 +42,57 @@ namespace SIS.WebServer
                     this.PrepareResponse(httpResponse);
                 }
             }
-            catch ()
+            catch (BadRequestException exception)
             {
-
-                throw;
+                this.PrepareResponse(new TextResult(exception.ToString(), HttpResponseStatusCode.BadRequest));
             }
+            catch (Exception e)
+            {
+                this.PrepareResponse(new TextResult(e.ToString(), HttpResponseStatusCode.InternalServerError));
+            }
+
+            this.client.Shutdown(SocketShutdown.Both);
         }
 
-        private HttpRequest ReadRequest()
+        private IHttpRequest ReadRequest()
         {
+            var result = new StringBuilder();
+            var data = new ArraySegment<byte>(new byte[1024]);
 
+            while (true)
+            {
+                int numberOfBytesRead = this.client.Receive(data.Array, SocketFlags.None);
+
+                if (numberOfBytesRead == 0)
+                {
+                    break;
+                }
+
+                var byteAsString = Encoding.UTF8.GetString(data.Array, 0, numberOfBytesRead);
+                result.Append(byteAsString);
+
+                if (numberOfBytesRead < 1023)
+                {
+                    break;
+                }
+            }
+
+            if (result.Length == 0)
+            {
+                return null;
+            }
+
+            return new HttpRequest(result.ToString());
         }
 
         private HttpResponse HandleRequest(IHttpRequest httpRequest)
         {
+            if (!this.serverRoutingTable.Contains(httpRequest.RequestMethod, httpRequest.Path))
+            {
+                return new TextResult($"Route with method {httpRequest.RequestMethod} and path \"{httpRequest.Path}\" not found.", HttpResponseStatusCode.NotFound);
+            }
 
+            return (HttpResponse)this.serverRoutingTable.Get(httpRequest.RequestMethod, httpRequest.Path).Invoke(httpRequest);
         }
 
         private void PrepareResponse(IHttpResponse httpResponse)
