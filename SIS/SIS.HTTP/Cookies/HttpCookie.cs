c397b574333efe231176af0cb549f20bb3399f27
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-17 09:56:54 +0300

    Update

---
+++ SIS/SIS.HTTP/Cookies/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookie
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime dateTime { get; set; }
+
+        public string Path { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public bool HttpOnly { get; set; }
+
+    }
+}
