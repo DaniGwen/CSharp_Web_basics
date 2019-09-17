b6cc2fee565b81346f339a2331ebf3ec08500ed0
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-17 14:16:03 +0300

    Adding Cookie support

--- SIS/SIS.HTTP/Cookies/HttpCookie.cs
+++ SIS/SIS.HTTP/Cookies/HttpCookie.cs
@@ -6,17 +6,38 @@ namespace SIS.HTTP.Cookies
 {
     public class HttpCookie
     {
+        private const int HttpCookieDefaultExpirationDay = 3;
+
+        private const string HttpCookieDefaultPath = "/";
+
+        public HttpCookie(string key, string value, int expires, string path)
+        {
+            expires = HttpCookieDefaultExpirationDay;
+            path = HttpCookieDefaultPath;
+        }
+
+        public HttpCookie(string key, string value, bool isNew, int expires, string path)
+        {
+            expires = HttpCookieDefaultExpirationDay;
+            path = HttpCookieDefaultPath;
+        }
+
         public string Key { get; set; }
 
         public string Value { get; set; }
 
-        public DateTime dateTime { get; set; }
+        public DateTime Expires { get; private set; }
 
         public string Path { get; set; }
 
         public bool IsNew { get; set; }
 
-        public bool HttpOnly { get; set; }
+        public bool HttpOnly { get; set; } = true;
+
+        public void Delete()
+        {
+
+        }
 
     }
 }
