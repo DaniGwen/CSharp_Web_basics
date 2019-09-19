9e36a630a182308a1ff1753a3f824ae83d945469
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-09-19 08:36:47 +0300

    Add Session namespace

---
+++ SIS/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Sessions
+{
+    public class HttpSession : IHttpSession
+    {
+        public string Id { get; }
+
+        private Dictionary<string, object> Sessions;
+
+        public HttpSession(string id)
+        {
+            this.Sessions
+        }
+        public void AddParameter(string name, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearParameter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetParameter(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
