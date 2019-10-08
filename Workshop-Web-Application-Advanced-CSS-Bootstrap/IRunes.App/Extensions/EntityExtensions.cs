feaedcb9283a07c37ef354b8947298afe3b73f5e
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-10-08 16:03:18 +0300

    Update
    Moved interfaces out of Contracts folder

--- Workshop-Web-Application-Advanced-CSS-Bootstrap/IRunes.App/Extensions/EntityExtensions.cs
+++ Workshop-Web-Application-Advanced-CSS-Bootstrap/IRunes.App/Extensions/EntityExtensions.cs
@@ -46,8 +46,8 @@ namespace IRunes.App.Extensions
         public static string ToHtmlDetails(this Track track)
         {
             return $"<div class=\"track-details text-center\">" +
-                   $"       <h1>Track Name: {WebUtility.UrlDecode(track.Name)}</h1>" +
-                   $"       <h1>Track Price: ${track.Price:F2}</h1>" +
+                   $"       <h3>Track Name: {WebUtility.UrlDecode(track.Name)}</h1>" +
+                   $"       <h3>Track Price: ${track.Price:F2}</h1>" +
                    "<hr class=\"bg-success w-50\" style=\"height: 2px\" />" +
                    $"     <iframe src=\"{WebUtility.UrlDecode(track.Link)}\" class=\"w-50 mx-auto\" height=\"400\"></iframe>" +
                    $"</div>";
