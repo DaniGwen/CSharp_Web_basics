a1523f96d64386641e2aad00d50ffaf294189412
Daniel G <dani.gwenski@gmail.com>
2019-10-28 21:11:23 +0200

    Update
    CompileAndInstance method done
    Update on GetCSharpCode in SisViewEngine

--- Workshop-Web-Application-Advanced-CSS-Bootstrap/SIS.WebServer/ViewEngine/SisViewEngine.cs
+++ Workshop-Web-Application-Advanced-CSS-Bootstrap/SIS.WebServer/ViewEngine/SisViewEngine.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace SIS.MvcFramework.ViewEngine
 {
@@ -53,13 +57,83 @@ namespace SIS.MvcFramework.ViewEngine
             }
 
             compilation = compilation.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(code));
-            compilation.Emit("a.dll");
-            return null;
+            using (var memoryStream = new MemoryStream())
+            {
+                var compilationResult = compilation.Emit(memoryStream);
+
+                if (!compilationResult.Success)
+                {
+                    foreach (var error in compilationResult.Diagnostics) //.Where(x=>x.Severity == DiagnosticSeverity.Error)
+                    {
+                        System.Console.WriteLine(error.GetMessage());
+                    }
+                    return null;
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var assemblyBytes = memoryStream.ToArray();
+                var assembly = Assembly.Load(assemblyBytes);
+
+                var type = assembly.GetType("AppViewCodeNamespace.AppViewCode");
+                if (type == null)
+                {
+                    System.Console.WriteLine("AppViewCode not found!");
+                    return null;
+                }
+
+                var instance = Activator.CreateInstance(type);
+                if (instance == null)
+                {
+                    Console.WriteLine("AppViewCode can not be instantiated");
+                    return null;
+                }
+                return instance as IView;
+            }
         }
 
         private string GetCSharpCode(string viewContent)
         {
-            return string.Empty;
+            string[] lines = viewContent.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            var cSharpCode = new StringBuilder();
+            var supportedOperators = new[] { "for", "if", "else" };
+
+            foreach (var line in lines)
+            {
+                if (line.TrimStart().StartsWith("{") || line.TrimStart().StartsWith("}"))
+                {
+                    // { / }
+                    cSharpCode.Append(line);
+                }
+                else if (supportedOperators.Any(x => line.TrimStart().StartsWith("@" + x)))
+                {
+                    // @C#
+                    var atSignLocation = line.IndexOf("@");
+                    var cSharpLine = line.Remove(atSignLocation, 1);
+                    cSharpCode.AppendLine(cSharpLine);
+                }
+                else
+                {
+                    //HTML
+                    if (!line.Contains("@"))
+                    {
+                        var cSharpLine = $"html.AppendLine(@\"{line.Replace("\"", "\"\"")}\");";
+                        cSharpCode.AppendLine(cSharpLine);
+                    }
+                    else
+                    {
+                        var cSharpStringToAppend = string.Empty;
+                        while (line.Contains("@"))
+                        {
+
+                        }
+                        cSharpStringToAppend = $"";
+                        var cSharpLine = $"html.AppendLine({cSharpStringToAppend});";
+                        cSharpCode.AppendLine(cSharpLine);
+                    }
+                  
+                }
+                return cSharpCode.ToString();
+            }
         }
     }
 }
